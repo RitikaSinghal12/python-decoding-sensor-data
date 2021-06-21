5b2949d0e8b578f1c97512afafb6c56a5f73cff3
RitikaSinghal12 <52667707+RitikaSinghal12@users.noreply.github.com>
2021-06-21 00:33:06 -0400

    2

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -4,9 +4,13 @@ def load_sensor_data():
     sensor_data = []
     sensor_files = glob.glob(os.path.join(os.getcwd(), "datasets", "*.csv"))
     for sensor_file in sensor_files:
-        with open(sensor_file) as data_file:
+        with open(sensor_file ) as data_file:
+            # Create a csv.DictReader
             data_reader = csv.DictReader(data_file, delimiter=',')
-            for row in data_file:
+            # Loop over each row dictionary
+            for row in data_reader:
+                # Create a list of dictionaries
                 sensor_data.append(row)
+
     return sensor_data
 
