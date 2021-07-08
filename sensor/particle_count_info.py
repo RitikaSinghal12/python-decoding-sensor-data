72afc87e8c16a3e733c20d98059435bafd4b12c4
RitikaSinghal12 <52667707+RitikaSinghal12@users.noreply.github.com>
2021-07-08 17:26:39 -0400

    6

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -16,14 +16,14 @@ class ParticleData(HouseInfo):
         recs = super().get_data_by_date("particulate", rec_date)
         return self._convert_data(recs)
 
-    def get_data_concetrations(self, data):
-        particulate = {"good":0, "moderate":0, "bad":0}
+    def get_data_concentrations(self, data):
+        particulate = {"good": 0, "moderate": 0, "bad": 0}
         for rec in data:
-            if(rec<=50.0):
-                particulate["good"]+1
-            elif(rec>50.0 and rec<=100.0):
-                particulate["moderate"]+1
+            if rec <= 50.0:
+                particulate["good"] += 1
+            elif rec > 50.0 and rec <= 100:
+                particulate["moderate"] += 1
             else:
-                particulate["bad"]+1
+                particulate["bad"] += 1
         return particulate
 
